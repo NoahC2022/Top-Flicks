0961ab1afca37d2ef7c3a8a21d662a4ce87dd43f
NoahC2022 <112981898+NoahC2022@users.noreply.github.com>
2023-05-25 19:08:26 -0700

    Initialized
    Everything works at this point, so I'm saving before playing around.

---
+++ src/app/about/page.jsx
@@ -0,0 +1,7 @@
+export default function About() {
+    return (
+        <main>
+            <h1>This is a side page</h1>
+        </main>
+    )
+}
---
+++ src/app/page.jsx
@@ -0,0 +1,23 @@
+import Movie from "./Movie"
+
+export default async function Home() {
+    const data = await fetch(`https://api.themoviedb.org/3/movie/popular?api_key=${process.env.API_KEY}`)
+    const res = await data.json()
+    console.log(res)
+
+    return (
+      <main>
+          <h1 className="text-lg py-2 m-4">These movies are the shit!</h1>
+          <h1 className="text-lg py-2 m-4">`{process.env.API_KEY}`</h1>
+            {res.results.map((movie) => (
+                <Movie
+                    key={movie.id}
+                    id={movie.id}
+                    title={movie.title}
+                    poster_path={movie.poster_path}
+                    release_date={movie.release_date}
+                    />
+          ))}
+      </main>
+  )
+}
