b8ab3329919b52f05696d575c599d35c2af432e2
NoahC2022 <112981898+NoahC2022@users.noreply.github.com>
2023-05-26 12:04:11 -0700

    Final Padding Fixed

--- src/app/layout.js
+++ src/app/layout.js
@@ -11,9 +11,6 @@ export const metadata = {
 export default function RootLayout({ children }) {
   return (
     <html lang="en">
-          <body className={`${inter.className} px-18`}>
-              {children}
-          </body>
       <body className={`${inter.className} px-12`}>{children}</body>
     </html>
   );
